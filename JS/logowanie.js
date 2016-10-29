05a5b7eafc274a57ae3735ce8c3551e73525b67e
KajkoIKokosz <pg1roboto@gmail.com>
2016-10-29 15:24:29 +0200

    zmiana czasu rozwijania. odstęp miedzy hasło i powtórz hasło

--- JS/logowanie.js
+++ JS/logowanie.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
    // event na buttonie 'Załóż Twitka'
     $('#createCount').on('click', function(){
             event.preventDefault();
-            $('.hiddenLoging').slideDown(500);
+            $('.hiddenLoging').slideDown(200);
            //$('#lub').css('display','none');
             $('#createCount').css('display','none');
             $('#twitaj').css('display','none');
